4369d779f2dfcd8c03cc554530f8573ca010b347
Guilherme Santana Pessa <guilhermesantanapessa12@gmail.com>
2023-12-09 23:03:37 -0300

    Movendo arquivo

--- Java/Arquivos/MoveFilesTest.java
+++ Java/Arquivos/MoveFilesTest.java
@@ -1,3 +1,28 @@
-public class MoveFilesTest {
+import java.nio.file.Files;
+import java.nio.file.Path;
 
+public class MoveFilesTest {
+    public static void main(String[] args) {
+        Path file = Path.of("moveFile.txt");
+
+        System.out.println("=========================================");
+
+        boolean existFile = Files.exists(file);
+        if (!existFile) {
+            try {
+                Files.createFile(file);
+            } catch (Exception error) {
+                error.getMessage();
+            }
+        }
+
+        try {
+            String fileName = file.getFileName().toString();
+            Files.move(file, Path.of("C:\\Users\\Gui\\OneDrive\\Anexos\\Área de Trabalho\\" + fileName));
+
+        } catch (Exception error) {
+            error.getMessage();
+        }
+        System.out.println("=========================================");
+    }
 }
