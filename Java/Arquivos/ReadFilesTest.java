317338490d54aebf8aef93f027fb5e729cb299ea
Guilherme Santana Pessa <guilhermesantanapessa12@gmail.com>
2023-12-11 17:29:19 -0300

    Maneiras de ler o conteudo de um arquivo

--- Java/Arquivos/ReadFilesTest.java
+++ Java/Arquivos/ReadFilesTest.java
@@ -1,23 +1,66 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Scanner;
 
 public class ReadFilesTest {
+    private final static String FILE = "readFile.txt";
+
     public static void main(String[] args) {
-        Path file = Path.of("cavalo.txt");
 
+        System.out.println("-------------------------------------------------------------------------------------");
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("Com InputStream");
+
+            System.out.println(bufferedReader.readLine());
+        } catch (Exception error) {
+            error.getMessage();
+        }
+
+        System.out.println("-------------------------------------------------------------------------------------");
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(FILE))) {
+            System.out.println("Com FileReader");
+
+            while (bufferedReader.ready()) {
+                System.out.println(bufferedReader.readLine());
+            }
+        } catch (Exception error) {
+            error.getMessage();
+        }
+
+        System.out.println("-------------------------------------------------------------------------------------");
+        try (Scanner scanner = new Scanner(Path.of(FILE))) {
+            System.out.println("Com Scanner");
+
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+        } catch (Exception error) {
+            error.getMessage();
+        }
+
+        System.out.println("-------------------------------------------------------------------------------------");
         try {
-            String text = Files.readString(file);
-            System.out.println(text);
+            System.out.println("Com String + Files");
 
-            // Scanner scanner = new Scanner(file);
-            // while (scanner.hasNextLine()) {
-            // System.out.println();
-            // String output = scanner.nextLine();
-            // System.out.println(output);
-            // }
+            String content = new String(Files.readAllBytes(Path.of(FILE)), StandardCharsets.UTF_8);
+            System.out.println(content);
+        } catch (Exception error) {
+            error.getMessage();
+        }
+
+        System.out.println("-------------------------------------------------------------------------------------");
+        try {
+            Path file = Path.of(FILE);
+            System.out.println("Com Files");
+            System.out.println(Files.readString(file));
         } catch (IOException error) {
-            error.printStackTrace();
+            error.getMessage();
         }
     }
 
