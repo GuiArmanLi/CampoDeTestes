7466f1546acc17fefc2a567dbc08e4d6eda82245
Guilherme <guilhermesantanapessa12@gmail.com>
2023-12-14 15:30:08 -0300

    Escrevendo em arquivos

--- Java/Arquivos/WriteFilesTest.java
+++ Java/Arquivos/WriteFilesTest.java
@@ -1,36 +1,53 @@
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Random;
 
 public class WriteFilesTest {
+    private static final String FILE = "writeFile.txt";
+
     public static void main(String[] args) {
-        Path file = Path.of("cavalo.txt");
+        Path file = Path.of(FILE);
 
-        try (BufferedWriter writer = Files.newBufferedWriter(file);
-                OutputStream outputStream = Files.newOutputStream(file)) {
-
-            if (!Files.exists(file)) {
-                file = Files.createFile(file);
-            }
-
-            // FileWriter fileWriter = new FileWriter(file);
-            // fileWriter.write(file.getAbsolutePath());
-            // fileWriter.close();
+        try (BufferedWriter writer = Files.newBufferedWriter(file)) {
+            writer.write("Escolha um cavalo para apostar");
+            writer.newLine();
 
             for (int i = 0; i < 10; i++) {
                 Random random = new Random();
-                writer.write("Cavalo n" + random.nextInt(0, 100));
+                writer.append("Cavalo n" + random.nextInt(0, 100));
                 writer.newLine();
             }
-
-            String text = Files.readString(file).toUpperCase();
-            System.out.println(text);
-
         } catch (IOException error) {
             error.getMessage();
+        }
+
+        try (OutputStreamWriter osw = new OutputStreamWriter(System.out);) {
+            osw.write("Hello World");
+            osw.write(111);
+        } catch (Exception error) {
+            error.getMessage();
+        }
+
+        try (FileWriter fl = new FileWriter(FILE, true)) {
+            fl.write("Same thing, but different");
+            fl.write(111);
+            fl.append("Isso esta appendando");
+        } catch (Exception error) {
+            System.out.println(error.getMessage());
+        }
+
+        try {
+            File files = new File("caralho.txt");
+            while (files.canWrite()) {
+                System.out.println("Do something!");
+            }
+        } catch (Exception error) {
+            System.out.println(error.getMessage());
         }
     }
 }
