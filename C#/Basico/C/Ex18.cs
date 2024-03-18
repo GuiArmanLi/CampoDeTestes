54b77d64835a4d1c68bdc9b9842e963c2db05535
Guilherme <guilhermesantanapessa12@gmail.com>
2024-03-17 23:26:21 -0300

    Exercicio Aula 13

---
+++ C#/Basico/C/Ex18.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Ex18
+{
+    static void Main(string[] args) {
+        Console.WriteLine("Digite uma data no seguinte formato: xx/xx/xxxx");
+        string date = Console.ReadLine();
+
+        string day = string
+        Console.WriteLine(day);
+
+
+    }
+}
