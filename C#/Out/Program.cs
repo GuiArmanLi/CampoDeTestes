720775425a6a70289ce81f8b57835a2b944dadac
Guilherme <guilhermesantanapessa12@gmail.com>
2024-07-23 01:58:15 -0300

    .

---
+++ C#/Out/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+Console.WriteLine("Digite o valor de um Raio: ");
+var raio = Double.Parse(Console.ReadLine());
+
+CalcularValoresDoCirculo(raio, out object diametro, out double circunferencia, out double area);
+
+Console.WriteLine(@$"
+    Raio: {raio} 
+    Diametro: {diametro}
+    Area: {area}"
+    );
+
+static void CalcularValoresDoCirculo(double raio, out object diametro, out double circunferencia, out double area)
+{
+    diametro = raio * 2;
+    circunferencia = 2 * Math.PI * raio;
+    area = Math.PI * raio * 2;
+}
